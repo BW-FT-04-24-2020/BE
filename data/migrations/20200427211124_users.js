acea5efaa1b31f9370de889d5d1d5feaccb5c30a
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-04-27 21:50:06 -0400

    create db for users

---
+++ data/migrations/20200427211124_users.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+        // primary key, id that autoincrements
+        tbl.increments('id');
+
+        tbl.string('username', 255)
+            .notNullable()
+            .unique()
+        
+        tbl.string('password', 255)
+            .notNullable()
+
+        tbl.string('email', 255)
+            .notNullable()
+            .unique()
+
+        tbl.string('name', 255)
+            .notNullable()
+
+        tbl.boolean('legal_age')
+            .defaultTo(false)
+            .notNullable()
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users');
+};
