00d54d80703bda62af700e5208efc408086cfc69
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-04-28 00:36:02 -0400

    stage

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 
 // routes import
 const usersRouter = require('./users/usersRouter');
+const authRouter = require('./auth/authRouter');
 
 const server = express();
 
@@ -15,5 +16,6 @@ server.get('/', (req, res) => {
     res.send('Hello World!!! Server up and running!!!');
 });
 server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 module.exports = server;
